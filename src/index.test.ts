99066b2b51d9a9a2fefb58287a73187515dbab50
Ferdinand von Hagen <ferdinand@vonhagen.me>
2023-10-09 19:33:02 +0200

    feat: also implement api gateway and sqs functionality
    This release adds more functionality to the framework to also process API Gateway and SQS events.
    Also adding a mode where we can simply register a provider for a specific type and request it. This will greatly simplify development for e.g. the auth email data and have it requested instead of needing to update it all the time...

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,12 @@
 import { Handler, Payload } from './index';
-import { KinesisStreamEvent } from 'aws-lambda';
+import {
+  APIGatewayProxyEventV2,
+  APIGatewayProxyResultV2,
+  KinesisStreamEvent,
+  SQSEvent,
+  SQSRecord,
+} from 'aws-lambda';
+import { resourceLimits } from 'worker_threads';
 
 describe('index', () => {
   let handler: Handler;
@@ -62,7 +69,7 @@ describe('index', () => {
         payload.decorations[0].timestamp = 1696529142788;
 
         expect(payload.stringify()).toBe(
-          '{"version":"v1","correlationId":"a7a54989-a7fc-471b-af20-89857bcd151e","publishTime":1696528221000,"saga":"stripe.customer","context":{"type":"stripe.customer","service":"stripe","timestamp":1696528321000,"payload":{"userId":"e762b0f3-7e11-47a4-99e5-32527d01b575","stripeId":"cus_OkWfnhA1cJGNWS"}},"decorations":[{"type":"auth.email","service":"test","timestamp":1696529142788,"payload":{"primaryEmail":"ferdinand.vonhagen@gonina.com","verified":true}}]}',
+          '{"version":"v1","correlationId":"a7a54989-a7fc-471b-af20-89857bcd151e","publishTime":1696528221000,"saga":"stripe.customer","context":{"type":"stripe.customer","service":"stripe","timestamp":1696528321000,"payload":{"userId":"e762b0f3-7e11-47a4-99e5-32527d01b575","stripeId":"cus_OkWfnhA1cJGNWS"}},"requested":[],"decorations":[{"type":"auth.email","service":"test","timestamp":1696529142788,"payload":{"primaryEmail":"ferdinand.vonhagen@gonina.com","verified":true}}]}',
         );
 
         const t1 = payload.getDecoration('auth.email');
@@ -112,6 +119,133 @@ describe('index', () => {
 
     const result = await handler.handler(event);
     expect(result).toBeDefined();
+    expect(called).toBe(false);
+  });
+
+  it('should happily process SQS events', async () => {
+    const event: SQSEvent = {
+      Records: [
+        {
+          messageId: '059f36b4-87a3-44ab-83d2-661975830a7d',
+          body: '{"foo":"bar"}',
+        } as SQSRecord,
+      ],
+    };
+
+    let called = false;
+    handler.registerSQSHandler(
+      async (data: any): Promise<Error | undefined> => {
+        called = true;
+        expect(data).toBeDefined();
+        expect(data.foo).toBe('bar');
+
+        return;
+      },
+    );
+
+    const result = await handler.handler(event);
+    expect(result).toBeDefined();
+    expect(result).toStrictEqual({ batchItemFailures: [] });
+    expect(called).toBe(true);
+  });
+
+  it('should happily process API gateway requests', async () => {
+    const event: APIGatewayProxyEventV2 = {
+      rawPath: '/test',
+    } as APIGatewayProxyEventV2;
+
+    let called = false;
+    handler.registerApiGatewayHandler(
+      async (
+        event: APIGatewayProxyEventV2,
+      ): Promise<APIGatewayProxyResultV2> => {
+        called = true;
+        return {
+          statusCode: 200,
+          body: 'test',
+        };
+      },
+    );
+
+    let result = await handler.handler(event);
+    expect(result).toBeDefined();
+    expect(result).toStrictEqual({
+      statusCode: 200,
+      body: 'test',
+    });
+    expect(called).toBe(true);
+  });
+
+  it('should be possible to process events on request (provider function)', async () => {
+    const noRequest =
+      'eyJ2ZXJzaW9uIjoidjEiLCJjb3JyZWxhdGlvbklkIjoiYTdhNTQ5ODktYTdmYy00NzFiLWFmMjAtODk4NTdiY2QxNTFlIiwicHVibGlzaFRpbWUiOjE2OTY1MjgyMjEwMDAsInNhZ2EiOiJzdHJpcGUuY3VzdG9tZXIiLCJjb250ZXh0Ijp7InR5cGUiOiJzdHJpcGUuY3VzdG9tZXIiLCJzZXJ2aWNlIjoic3RyaXBlIiwidGltZXN0YW1wIjoxNjk2NTI4MzIxMDAwLCJwYXlsb2FkIjp7InVzZXJJZCI6ImU3NjJiMGYzLTdlMTEtNDdhNC05OWU1LTMyNTI3ZDAxYjU3NSIsInN0cmlwZUlkIjoiY3VzX09rV2ZuaEExY0pHTldTIn19LCJkZWNvcmF0aW9ucyI6W119';
+    const request =
+      'eyJ2ZXJzaW9uIjoidjEiLCJjb3JyZWxhdGlvbklkIjoiYTdhNTQ5ODktYTdmYy00NzFiLWFmMjAtODk4NTdiY2QxNTFlIiwicHVibGlzaFRpbWUiOjE2OTY1MjgyMjEwMDAsInNhZ2EiOiJzdHJpcGUuY3VzdG9tZXIiLCJjb250ZXh0Ijp7InR5cGUiOiJzdHJpcGUuY3VzdG9tZXIiLCJzZXJ2aWNlIjoic3RyaXBlIiwidGltZXN0YW1wIjoxNjk2NTI4MzIxMDAwLCJwYXlsb2FkIjp7InVzZXJJZCI6ImU3NjJiMGYzLTdlMTEtNDdhNC05OWU1LTMyNTI3ZDAxYjU3NSIsInN0cmlwZUlkIjoiY3VzX09rV2ZuaEExY0pHTldTIn19LCJkZWNvcmF0aW9ucyI6W10sInJlcXVlc3RlZCI6WyJ0ZXN0Il19';
+    const requestFulfilled =
+      'eyJ2ZXJzaW9uIjoidjEiLCJjb3JyZWxhdGlvbklkIjoiYTdhNTQ5ODktYTdmYy00NzFiLWFmMjAtODk4NTdiY2QxNTFlIiwicHVibGlzaFRpbWUiOjE2OTY1MjgyMjEwMDAsInNhZ2EiOiJzdHJpcGUuY3VzdG9tZXIiLCJjb250ZXh0Ijp7InR5cGUiOiJzdHJpcGUuY3VzdG9tZXIiLCJzZXJ2aWNlIjoic3RyaXBlIiwidGltZXN0YW1wIjoxNjk2NTI4MzIxMDAwLCJwYXlsb2FkIjp7InVzZXJJZCI6ImU3NjJiMGYzLTdlMTEtNDdhNC05OWU1LTMyNTI3ZDAxYjU3NSIsInN0cmlwZUlkIjoiY3VzX09rV2ZuaEExY0pHTldTIn19LCJkZWNvcmF0aW9ucyI6W3sidHlwZSI6InRlc3QiLCJzZXJ2aWNlIjoic29tZXRoaW5nIn1dLCJyZXF1ZXN0ZWQiOlsidGVzdCIsICJ0ZXN0MiJdfQ==';
+
+    const event1: KinesisStreamEvent = {
+      Records: [
+        {
+          kinesis: {
+            sequenceNumber: '3247',
+            data: noRequest,
+          },
+        },
+      ],
+    } as unknown as KinesisStreamEvent;
+
+    let called = false;
+    handler.registerProvider("test", async (event: Payload): Promise<any> => {
+      called = true;
+      return new Error("hello world");
+    });
+
+    let result = await handler.handler(event1);
+    expect(result).toBeDefined();
+    expect(result).toStrictEqual({
+      "batchItemFailures": [],
+    });
+    expect(called).toBe(false);
+
+    const event2: KinesisStreamEvent = {
+      Records: [
+        {
+          kinesis: {
+            sequenceNumber: '3247',
+            data: request,
+          },
+        },
+      ],
+    } as unknown as KinesisStreamEvent;
+    result = await handler.handler(event2);
+
+    expect(result).toBeDefined();
+    expect(result).toStrictEqual({
+      "batchItemFailures": [{
+        "itemIdentifier": "3247",
+      }],
+    });
+    expect(called).toBe(true);
+
+    const event3: KinesisStreamEvent = {
+      Records: [
+        {
+          kinesis: {
+            sequenceNumber: '3247',
+            data: requestFulfilled,
+          },
+        },
+      ],
+    } as unknown as KinesisStreamEvent;
+
+    called = false;
+    result = await handler.handler(event3);
+
+    expect(result).toBeDefined();
+    expect(result).toStrictEqual({
+      "batchItemFailures": [],
+    });
     expect(called).toBe(false);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import {
+  APIGatewayProxyEventV2,
+  APIGatewayProxyResultV2,
   KinesisStreamBatchItemFailure,
   KinesisStreamBatchResponse,
   KinesisStreamEvent,
   KinesisStreamRecord,
+  SQSBatchItemFailure,
+  SQSEvent,
+  SQSRecord,
 } from 'aws-lambda';
 import {
   Kinesis,
@@ -31,6 +36,7 @@ export class Payload {
   publishTime: number;
   saga: string;
   context: Decoration;
+  requested: string[];
   decorations: Decoration[];
   private _service: string;
 
@@ -42,6 +48,7 @@ export class Payload {
     this.saga = saga || '';
     this.context = context || new Decoration();
 
+    this.requested = [];
     this.decorations = [];
 
     this._service = '';
@@ -50,10 +57,11 @@ export class Payload {
   static fromJSON(json: string): Payload {
     const payload = JSON.parse(json);
     const p = new Payload(payload.saga, payload.context);
-    p.version = payload.version;
-    p.correlationId = payload.correlationId;
-    p.publishTime = payload.publishTime;
-    p.decorations = payload.decorations;
+    p.version = payload.version || 'v0';
+    p.correlationId = payload.correlationId || uuidv4();
+    p.publishTime = payload.publishTime || Date.now();
+    p.requested = payload.requested || [];
+    p.decorations = payload.decorations || [];
 
     return p;
   }
@@ -81,6 +89,10 @@ export class Payload {
     this.decorations.push(new Decoration(type, this._service, payload));
   }
 
+  hasDecoration(type: string) {
+    return this.decorations.some((d) => d.type === type);
+  }
+
   wasProcessedByService(service: string) {
     return (
       this.context.service == service ||
@@ -96,6 +108,10 @@ export class Payload {
     return (this.decorations || []).find((d) => d.type === type);
   }
 
+  nextRequest(): string | undefined {
+    return this.requested.find((r) => !this.hasDecoration(r));
+  }
+
   stringify(): string {
     return JSON.stringify(this, (key, value) => {
       if (key.startsWith('_')) {
@@ -107,6 +123,11 @@ export class Payload {
 }
 
 export type RecordHandler = (payload: Payload) => Promise<Error | undefined>;
+export type SQSHandler = (payload: SQSRecord) => Promise<Error | undefined>;
+export type ProviderHandler = (payload: Payload) => Promise<any>;
+export type ApiGatewayHandler = (
+  payload: APIGatewayProxyEventV2,
+) => Promise<APIGatewayProxyResultV2>;
 
 class HandlerError extends Error {
   public readonly sequenceNumber: string;
@@ -121,10 +142,14 @@ export class Handler {
   private readonly serviceName: string;
   private readonly kinesis: Kinesis;
   private handlers: Map<string, RecordHandler>;
+  private providers: Map<string, ProviderHandler>;
+  private sqsHandler?: SQSHandler;
+  private apiGatewayHandler?: ApiGatewayHandler;
 
   constructor(serviceName: string) {
     this.serviceName = serviceName;
     this.handlers = new Map<string, RecordHandler>();
+    this.providers = new Map<string, ProviderHandler>();
 
     this.kinesis = new Kinesis();
   }
@@ -133,9 +158,32 @@ export class Handler {
     this.handlers.set(saga, handler);
   }
 
+  public registerProvider(provider: string, handler: ProviderHandler) {
+    this.providers.set(provider, handler);
+  }
+
+  public registerApiGatewayHandler(handler: ApiGatewayHandler) {
+    this.apiGatewayHandler = handler;
+  }
+
+  public registerSQSHandler(handler: SQSHandler) {
+    this.sqsHandler = handler;
+  }
+
   public async handler(
-    request: KinesisStreamEvent,
-  ): Promise<KinesisStreamBatchResponse> {
+    request: KinesisStreamEvent | SQSEvent | APIGatewayProxyEventV2,
+  ): Promise<
+    KinesisStreamBatchResponse | SQSBatchItemFailure | APIGatewayProxyResultV2
+  > {
+    if (!request.hasOwnProperty('Records')) {
+      if (!this.apiGatewayHandler) {
+        throw new Error('No ApiGateway handler registered');
+      }
+
+      return await this.apiGatewayHandler(request as APIGatewayProxyEventV2);
+    }
+
+    request = request as KinesisStreamEvent | SQSEvent;
     const results = await Promise.all(
       request.Records.map(this.handleRecord.bind(this)),
     );
@@ -151,28 +199,63 @@ export class Handler {
   }
 
   async handleRecord(
-    rec: KinesisStreamRecord,
+    rec: KinesisStreamRecord | SQSRecord,
   ): Promise<HandlerError | undefined> {
+    if (rec.hasOwnProperty('messageId')) {
+      rec = rec as SQSRecord;
+
+      if (!this.sqsHandler) {
+        return new HandlerError(rec.messageId, 'No SQS handler registered');
+      }
+
+      const res = await this.sqsHandler(JSON.parse(rec.body));
+      if (res) {
+        console.log('handler error', res);
+        return new HandlerError(rec.messageId, res.message);
+      }
+
+      return;
+    }
+
+    rec = rec as KinesisStreamRecord;
+
     const data = Buffer.from(rec.kinesis.data, 'base64').toString('utf-8');
     const payload = Payload.fromJSON(data);
 
     // We validate that the service has not processed this message before and that the version is correct.
     if (!payload.validateAndSet(this.serviceName)) {
-      console.log('Message not processed by service', payload);
       return;
     }
 
     const handler = this.handlers.get(payload.saga);
-    if (!handler) {
-      console.log('No handler for message', payload);
+    if (handler) {
+      const res = await handler(payload);
+      if (res) {
+        console.log('handler error', res);
+        return new HandlerError(rec.kinesis.sequenceNumber, res.message);
+      }
+
       return;
     }
 
-    const res = await handler(payload);
-    if (res) {
-      console.log('handler error', res);
+    const next = payload.nextRequest();
+    if (!next) {
+      return;
+    }
+
+    const provider = this.providers.get(next);
+    if (!provider) {
+      return;
+    }
+
+    const res = await provider(payload);
+    if (res instanceof Error) {
+      console.log('provider error', res);
       return new HandlerError(rec.kinesis.sequenceNumber, res.message);
     }
+
+    payload.decorate(next, res);
+    await this.publish(payload);
 
     return;
   }
